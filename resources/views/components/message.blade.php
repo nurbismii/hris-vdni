22f39688f883cc2f6fbdae126af52143fe6db585
nurbismii <nurbismi74@gmail.com>
2023-04-01 09:04:25 +0800

    Update

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,51 +1,39 @@
 @if ($message = Session::get('success'))
-
 <div class="alert alert-success alert-dismissible fade show" role="alert">
-  <h6 class="alert-heading">Success</h6>
-  {{ $message }}
-  <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+  <strong>Success</strong> {{ $message }}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
-
 @endif
 
 
 @if ($message = Session::get('error'))
-
 <div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <h6 class="alert-heading">Failed</h6>
-  {{ $message }}
-  <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+  <strong>Error</strong> {{ $message}}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
-
 @endif
 
 
 @if ($message = Session::get('warning'))
-
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-  <h6 class="alert-heading">Warning</h6>
-  {{ $message }}
-  <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Warning</strong> {{ $message}}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
-
 @endif
+
 
 
 @if ($message = Session::get('info'))
 <div class="alert alert-info alert-dismissible fade show" role="alert">
-  <h6 class="alert-heading">Information</h6>
-  {{ $message }}
-  <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+  <strong>Info</strong> {{ $message}}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 @endif
 
 
 @if (count($errors) > 0)
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <h6 class="alert-heading">Something wrong!</h6>
-  @foreach($errors->all() as $error)
-  {{ $error }} <br>
-  @endforeach
-  <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Something wrong!</strong> {{ $errors->first() }}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 @endif
