4e620832ff6e8a87dd98e45e662d5c781679a197
nurbismii <nurbismi74@gmail.com>
2023-07-27 10:17:01 +0800

    [fix] Export divisi

--- app/Exports/DivisiExport.php
+++ app/Exports/DivisiExport.php
@@ -2,16 +2,16 @@
 
 namespace App\Exports;
 
-use App\Models\divisi;
+use App\Models\Divisi;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
 class DivisiExport implements FromCollection
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
-        return divisi::all();
+        return Divisi::all();
     }
 }
