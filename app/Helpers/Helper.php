6a7990a2ec55f4dbb2070b13768456e042449f69
nurbismii <nurbismi74@gmail.com>
2023-11-21 11:51:05 +0800

    test: Test Dashboard

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -63,6 +63,7 @@ if (!function_exists('getNamaKecamatan')) {
 if (!function_exists('getNamaKelurahan')) {
     function getNamaKelurahan($key)
     {
+        return $key;
         $data = Kelurahan::where('id', $key)->first();
         if ($data) {
             return $data->kelurahan;
