01f653c35cc46e2fcd4ad604e32bf7f02c733726
nurbismii <nurbismi74@gmail.com>
2024-05-16 09:51:45 +0800

    test: view email konfirmasi

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -24,7 +24,7 @@
                                             <tr>
                                                 <td style="padding-top: 60px; padding-bottom: 20px;" align="center" valign="middle" class="emailLogo">
                                                     <a href="#" style="text-decoration:none" target="_blank">
-                                                        <img alt="" border="0" src="http://email.aumfusion.com/vespro/img/hero-img/blue/logo.png" style="width:100%;max-width:150px;height:auto;display:block" width="150">
+                                                        <img alt="" border="0" src="{{ asset('assets/img/backgrounds/vdni-ikon.png') }}" style="width:100%;max-width:150px;height:auto;display:block" width="150">
                                                     </a>
                                                 </td>
                                             </tr>
@@ -37,7 +37,7 @@
                                             </tr>
                                             <tr>
                                                 <td style="padding-bottom: 5px; padding-left: 20px; padding-right: 20px;" align="center" valign="top" class="mainTitle">
-                                                    <h2 class="text" style="color:#000;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:28px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:36px;text-transform:none;text-align:center;padding:0;margin:0">Hi "{{ ucwords(getName('nik_karyawan')) }}"</h2>
+                                                    <h2 class="text" style="color:#000;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:28px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:36px;text-transform:none;text-align:center;padding:0;margin:0">Hi "{{ getName($data['nik_karyawan']) }}"</h2>
                                                 </td>
                                             </tr>
                                             <tr>
@@ -63,7 +63,7 @@
                                                                     <table border="0" cellpadding="0" cellspacing="0" align="center">
                                                                         <tbody>
                                                                             <tr>
-                                                                                <td style="background-color: rgb(0, 210, 244); padding: 12px 35px; border-radius: 50px;" align="center" class="ctaButton"> <a href="#" style="color:#fff;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:13px;font-weight:600;font-style:normal;letter-spacing:1px;line-height:20px;text-transform:uppercase;text-decoration:none;display:block" target="_blank" class="text">Confirm Email</a>
+                                                                                <td style="background-color: rgb(0, 210, 244); padding: 12px 35px; border-radius: 50px;" align="center" class="ctaButton"> <a href="{{ route('email.confirm', $data['id']) }}" style="color:#fff;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:13px;font-weight:600;font-style:normal;letter-spacing:1px;line-height:20px;text-transform:uppercase;text-decoration:none;display:block" target="_blank" class="text">Confirm Email</a>
                                                                                 </td>
                                                                             </tr>
                                                                         </tbody>
@@ -81,7 +81,7 @@
                                                 <td align="center" valign="middle" style="padding-bottom: 40px;" class="emailRegards">
                                                     <!-- Image and Link // -->
                                                     <a href="#" target="_blank" style="text-decoration:none;">
-                                                        <img mc:edit="signature" src="http://email.aumfusion.com/vespro/img//other/signature.png" alt="" width="150" border="0" style="width:100%; max-width:150px; height:auto; display:block;">
+                                                        <img mc:edit="signature" src="{{ asset('assets/img/backgrounds/icon.png') }}" alt="" width="150" border="0" style="width:100%; max-width:150px; height: 100px; display:block;">
                                                     </a>
                                                 </td>
                                             </tr>
