3cac4936d153d364601f0b815b0c415e2f7f81ce
Nurbismii <nurbismi74@gmail.com>
2024-07-31 10:22:29 +0800

    test: check exist data resign

--- app/Imports/ResignImport.php
+++ app/Imports/ResignImport.php
@@ -19,18 +19,21 @@ class ResignImport implements ToCollection, WithHeadingRow, WithValidation
     public function collection(Collection $collection)
     {
         foreach ($collection as $collect) {
-            $datas[] = [
-                'no_surat' => $collect['no_surat'],
-                'nik_karyawan' => $collect['nik_karyawan'],
-                'no_ktp' => $collect['no_ktp'],
-                'tanggal_pengajuan' => Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject(intVal($collect['tanggal_pengajuan']))),
-                'tanggal_keluar' => Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject(intVal($collect['tanggal_keluar']))),
-                'alasan_keluar' => $collect['alasan_keluar'],
-                'tipe' => strtoupper($collect['tipe']),
-                'periode_awal' => Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject(intVal($collect['periode_awal']))),
-                'periode_akhir' => Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject(intVal($collect['periode_akhir']))),
-                'created_at' =>  Carbon::now()->format('Y-m-d H:i:s'),
-            ];
+            $check_exist = Resign::where('nik_karyawan', $collect['nik_karyawan'])->first();
+            if (!$check_exist) {
+                $datas[] = [
+                    'no_surat' => $collect['no_surat'],
+                    'nik_karyawan' => $collect['nik_karyawan'],
+                    'no_ktp' => $collect['no_ktp'],
+                    'tanggal_pengajuan' => Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject(intVal($collect['tanggal_pengajuan']))),
+                    'tanggal_keluar' => Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject(intVal($collect['tanggal_keluar']))),
+                    'alasan_keluar' => $collect['alasan_keluar'],
+                    'tipe' => strtoupper($collect['tipe']),
+                    'periode_awal' => Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject(intVal($collect['periode_awal']))),
+                    'periode_akhir' => Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject(intVal($collect['periode_akhir']))),
+                    'created_at' =>  Carbon::now()->format('Y-m-d H:i:s'),
+                ];
+            }
         }
 
         foreach (array_chunk($datas, 300) as $chunk) {
